850b53b0ef9230de363df38776c9ab4c0315102c
MrT3313 <turgeon.dev@gmail.com>
2020-03-24 16:55:57 -0500

    heroku live DB debug

--- backend/api/routes/login_route.js
+++ backend/api/routes/login_route.js
@@ -18,9 +18,18 @@ const sign_JWT = require('../../utils/sign_JWT.js')
         // - 1 - //
         router.get('/', async(req,res) => {
             console.log('** LOGIN ROUTE: TEST GET/ **')
-            res.status(200).json({
-                message: 'TEST GET request for LOGIN ROUTE working'
-            })
+            // res.status(200).json({
+            //     message: 'TEST GET request for LOGIN ROUTE working'
+            // })
+            KNEX_DB('users')
+                .then(allUsers => {
+                    res.status(200).json(allUsers)
+                })
+                .catch( err => {
+                    res.status(500).json({
+                        error: "Unabel to get all users"
+                    })
+                })
         })
     // - POST - //
         /* ACCEPTED SHAPE
