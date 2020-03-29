86c7c92e2ef1022ee9114986b73acbb1cdc3283e
MrT3313 <turgeon.dev@gmail.com>
2020-03-29 14:16:09 -0500

    DB Tables Updated
    Books - Title & Author
    ReadHistory - ** many to many ** userID bookID

---
+++ backend/data/migrations/20200329134650_READHISTORY_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('readHistory', tbl => {
+        tbl.increments('id')
+        tbl.integer('userID')
+            .references('id').inTable('users')
+            .onDelete('NO ACTION')
+            .onUpdate("CASCADE");
+        
+        tbl.integer('bookID')
+            .references('id').inTable('books')
+            .onDelete('NO ACTION')
+            .onUpdate("CASCADE");
+        tbl.timestamps(true, true)
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('readHistory')
+};
