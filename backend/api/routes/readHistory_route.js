11469f32417dc5241d84c3e43d445e3f2d3e7f69
MrT3313 <turgeon.dev@gmail.com>
2020-03-30 20:55:14 -0500

    readHistory_route working

--- backend/api/routes/readHistory_route.js
+++ backend/api/routes/readHistory_route.js
@@ -22,6 +22,25 @@ const router = express.Router()
             })
         })
     // - POST - // 
+        /* ACCEPTED SHAPE
+            {
+                "userID": INTEGER [fk],
+                "bookID": INTEGER [fk],
+            } 
+        */
+        router.post('/completedBook', async(req,res) => {
+        console.log('** READ HISTORY ROUTE: /log/completedBook')
+        // -- //
+            KNEX_DB('readHistory').insert(req.body)
+                .then( results => {
+                console.log(results)
+                // -- //
+                    res.status(200).json(results)
+                })
+                .catch( err => {
+                    res.status(500).json({ ERROR: 'Unable to log your accomplishment'})
+                })
+        })
     // - PUT - // 
     // - DEL - // 
 
