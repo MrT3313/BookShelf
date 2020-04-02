2370348efcb9a3a4335fa1edb77bf61afb659d91
MrT3313 <turgeon.dev@gmail.com>
2020-04-01 19:55:55 -0500

    GET/ reviews/all & POST/ reviews/

---
+++ backend/data/migrations/20200401193229_REVIEWS_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('reviews', tbl => {
+        tbl.increments('id')
+        tbl.integer('userID')
+            .references('id').inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate("CASCADE");
+        tbl.integer('bookID')
+            .references('id').inTable('books')
+            .onDelete('CASCADE')
+            .onUpdate("CASCADE");
+        
+        tbl.string('review').notNullable()
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('reviews')
+};
