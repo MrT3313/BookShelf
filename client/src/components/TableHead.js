40fe4e65df8dea1dbd87b535c6f3a4a7e57ce95b
MrT3313 <turgeon.dev@gmail.com>
2020-04-10 14:32:25 -0500

    <TableHead /> Update

--- client/src/components/TableHead.js
+++ client/src/components/TableHead.js
@@ -25,52 +25,56 @@ import FilterListIcon from '@material-ui/icons/FilterList';
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- //
   
 export function EnhancedTableHead(props) {
-    const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
+// -- // 
+    // Data
+    const headCells = [
+        { id: 'title', numeric: false, disablePadding: true, label: 'Title' },
+        { id: 'author', numeric: true, disablePadding: false, label: 'Author' },
+        // { id: 'date', numeric: true, disablePadding: false, label: 'Date' },
+        // { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+        // { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+    ];
+
+    //  Methods
     const createSortHandler = (property) => (event) => {
         onRequestSort(event, property);
     };
 
-    const headCells = [
-        { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-        { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-        { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-        { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-        { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
-    ];
-
+    // Return
     return (
         <TableHead>
-        <TableRow>
-            <TableCell padding="checkbox">
-            <Checkbox
-                indeterminate={numSelected > 0 && numSelected < rowCount}
-                checked={rowCount > 0 && numSelected === rowCount}
-                onChange={onSelectAllClick}
-                inputProps={{ 'aria-label': 'select all desserts' }}
-            />
-            </TableCell>
-            {headCells.map((headCell) => (
-            <TableCell
-                key={headCell.id}
-                align={headCell.numeric ? 'right' : 'left'}
-                padding={headCell.disablePadding ? 'none' : 'default'}
-                sortDirection={orderBy === headCell.id ? order : false}
-            >
-                <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : 'asc'}
-                onClick={createSortHandler(headCell.id)}
-                >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                    <span className={classes.visuallyHidden}>
-                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                    </span>
-                ) : null}
-                </TableSortLabel>
-            </TableCell>
-            ))}
-        </TableRow>
+            <TableRow>
+                <TableCell padding="checkbox">
+                    <Checkbox
+                        indeterminate={numSelected > 0 && numSelected < rowCount}
+                        checked={rowCount > 0 && numSelected === rowCount}
+                        onChange={onSelectAllClick}
+                        inputProps={{ 'aria-label': 'select all logs' }}
+                    />
+                </TableCell>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'default'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                        {headCell.label}
+                        {orderBy === headCell.id ? (
+                            <span className={classes.visuallyHidden}>
+                                {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                            </span>
+                        ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
         </TableHead>
     );
 }
