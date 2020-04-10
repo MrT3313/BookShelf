cfbf20b6cc7e5084d7b2c1e433ce84f60f7edabc
MrT3313 <turgeon.dev@gmail.com>
2020-04-10 14:39:40 -0500

    <Table /> Update

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -28,6 +28,7 @@ import { EnhancedTableToolbar } from '../components/TableToolBar.js'
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- //
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- //
 
+// Styles
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
@@ -52,47 +53,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-      return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-      return 1;
-    }
-    return 0;
-}
-  
-  function getComparator(order, orderBy) {
-    return order === 'desc'
-      ? (a, b) => descendingComparator(a, b, orderBy)
-      : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
-
-
+// __MAIN__
 export default function LogTable() {
-    const classes = useStyles();
+  // State
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('calories');
     const [selected, setSelected] = React.useState([]);
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  
+  // Styles
+    const classes = useStyles();
 
-    // ---- DATA ----  //
-    // ---- DATA ----  //
+  // ---- DATA ----  //
+  // ---- DATA ----  //
     function createData(name, calories, fat, carbs, protein) {
         return { name, calories, fat, carbs, protein };
     }
-  
+
     const rows = [
         createData('Cupcake', 305, 3.7, 67, 4.3),
         createData('Donut', 452, 25.0, 51, 4.9),
@@ -108,57 +86,93 @@ export default function LogTable() {
         createData('Nougat', 360, 19.0, 9, 37.0),
         createData('Oreo', 437, 18.0, 63, 4.0),
     ];
-    // ---- DATA ----  //
-    // ---- DATA ----  //
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
+  // ---- DATA ----  //
+  // ---- DATA ----  //
+  // ---- COMPARISON ---- //
+  // ---- COMPARISON ---- //
+    function descendingComparator(a, b, orderBy) {
+      if (b[orderBy] < a[orderBy]) {
+        return -1;
+      }
+      if (b[orderBy] > a[orderBy]) {
+        return 1;
+      }
+      return 0;
     }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
+    
+    function getComparator(order, orderBy) {
+      return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+  
+    function stableSort(array, comparator) {
+        const stabilizedThis = array.map((el, index) => [el, index]);
+        stabilizedThis.sort((a, b) => {
+            const order = comparator(a[0], b[0]);
+            if (order !== 0) return order;
+            return a[1] - b[1];
+        });
+        return stabilizedThis.map((el) => el[0]);
     }
 
-    setSelected(newSelected);
-  };
+    const handleRequestSort = (event, property) => {
+      const isAsc = orderBy === property && order === 'asc';
+      setOrder(isAsc ? 'desc' : 'asc');
+      setOrderBy(property);
+    };
+  // ---- COMPARISON ---- //
+  // ---- COMPARISON ---- //
+  // ---- SELECTION ---- //
+  // ---- SELECTION ---- //
+    const handleSelectAllClick = (event) => {
+      if (event.target.checked) {
+        const newSelecteds = rows.map((n) => n.name);
+        setSelected(newSelecteds);
+        return;
+      }
+      setSelected([]);
+    };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+    const handleClick = (event, name) => {
+      const selectedIndex = selected.indexOf(name);
+      let newSelected = [];
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+      if (selectedIndex === -1) {
+        newSelected = newSelected.concat(selected, name);
+      } else if (selectedIndex === 0) {
+        newSelected = newSelected.concat(selected.slice(1));
+      } else if (selectedIndex === selected.length - 1) {
+        newSelected = newSelected.concat(selected.slice(0, -1));
+      } else if (selectedIndex > 0) {
+        newSelected = newSelected.concat(
+          selected.slice(0, selectedIndex),
+          selected.slice(selectedIndex + 1),
+        );
+      }
 
-  const isSelected = (name) => selected.indexOf(name) !== -1;
+      setSelected(newSelected);
+    };
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+  // ---- SELECTION ---- //
+  // ---- SELECTION ---- //
+  // ---- PAGE / ROWS ---- //
+  // ---- PAGE / ROWS ---- //
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
 
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(parseInt(event.target.value, 10));
+      setPage(0);
+    };
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  // ---- PAGE / ROWS ---- //
+  // ---- PAGE / ROWS ---- //
+
+  // Return
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
