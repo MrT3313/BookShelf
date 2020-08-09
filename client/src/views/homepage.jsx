9500ab1b4cea7cd374d6b38c3244d99474500118
MrT3313 <turgeon.dev@gmail.com>
2020-08-09 18:36:02 -0400

    Removed Unused Tab Setups

--- client/src/views/homepage.jsx
+++ client/src/views/homepage.jsx
@@ -13,9 +13,6 @@ import { makeStyles } from '@material-ui/core/styles';
 // COMPONENTS
 import MenuAppBar from '../components/appBar/AppBar.js'
 import ListSelector from '../components/homepage/explore_NYT_booksAPI/ListSelector.js'
-
-import Explorer from '../components/homepage/explore_NYT_booksAPI/listExplorer/Tabs.js' 
-import ReactTabs from '../components/homepage/explore_NYT_booksAPI/reactTabs/Tabs.js'
 import HorizontalTabs from '../components/homepage/explore_NYT_booksAPI/MaterialUI_HorizontalTabs/Tabs.js'
 
 // ACTION CREATORS
@@ -84,8 +81,6 @@ const {
             <>
                 <MenuAppBar />
                 <ListSelector />
-                {/* <Explorer /> */}
-                {/* <ReactTabs />  */}
                 <HorizontalTabs />
             </>
         )
